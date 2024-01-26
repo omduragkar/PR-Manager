3dcf123a776ef2fe99b06694ac0915d6ad9fa598
Om Duragkar <omduragkar@Oms-MacBook-Pro.local>
2024-01-26 18:11:13 +0530

    Deployment backend url mertrics

--- src/utils/AxiosSetup.js
+++ src/utils/AxiosSetup.js
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 export const contentAxios = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://doubtful-deer-shoe.cyclic.app",
 });
 export const generateAxiosError = (message, config) => {
   const networkError = new Error(message);
